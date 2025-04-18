854d69844bf3d32c8fa5aa227da8055127e036c5
Julia Hehl <julia.hehl@bmw.de>
2025-04-18 16:43:49 +0200

    Adding method to encode features

--- src/preprocessing/preprocess_data.py
+++ src/preprocessing/preprocess_data.py
@@ -15,7 +15,7 @@ from data_cleaning import (
     filter_invalid_trips,
 )
 from geo_spatial import calculate_polyline_features, extract_lat_lon
-from utils import convert_datatypes
+from utils import convert_datatypes, extend_timestamps, reduce_high_cardinality, feature_encoding_oh, feature_encoding_oe
 
 
 class BasePreprocessor():
@@ -23,24 +23,6 @@ class BasePreprocessor():
         self.n_min_points = n_min_points
         self.id_column = id_column
         self.model_columns = model_columns
-
-    def preprocess_data(self, data: pd.DataFrame, is_train_data: bool) -> pd.DataFrame:
-        data = convert_polyline_to_geojson_format(data)
-        data = calculate_polyline_features(data)
-        data = filter_invalid_trips(data, n_points=self.n_min_points, id_column=self.id_column,
-                                    column_coordinate_points='n_coordinate_points')
-        data['sequence'] = data['polyline'].apply(lambda row: np.hstack(row))
-        if is_train_data:
-            data = data[(data.n_coordinate_points <= data.n_coordinate_points.quantile(0.90))
-                 & (data.total_distance_km <= data.total_distance_km.quantile(0.90))]
-            data = data[data.missing_data == False]
-        model_columns = self.model_columns.append(self.id_column)
-        data = data[[model_columns]]
-        return data
-
-class MulitoutputNNPreprocessor(BasePreprocessor):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     def preprocess_data(self, data: pd.DataFrame, is_training: bool) -> pd.DataFrame:
         data = convert_polyline_to_geojson_format(data)
@@ -56,6 +38,27 @@ class MulitoutputNNPreprocessor(BasePreprocessor):
         data = data[[model_columns]]
         return data
     
+    def encode_features(self, data: pd.DataFrame, is_training: bool) -> pd.DataFrame:
+        data = extend_timestamps(data, timestamp_column='timestamp')
+        data = reduce_high_cardinality(data, columns=['origin_stand'])
+        features_oh = ['call_type','origin_stand_agg','year_month']
+        df_fenc_oh = feature_encoding_oh(data, features_oh)
+        data = pd.concat([data, df_fenc_oh],axis=1)
+        #TODO: fix 
+        #if not is_training:
+         #   data = data.add_binary_features(data, test_data)
+            
+        # non_features = [non_feature.lower() for non_feature in non_features]
+        return data 
+
+class MulitoutputNNPreprocessor(BasePreprocessor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def preprocess_data(self, data: pd.DataFrame, is_training: bool) -> pd.DataFrame:
+        data = super().preprocess_data(data.copy(), is_training=is_training)
+        return data
+    
 
 class ClusteringPreprocessor(BasePreprocessor):
     def __init__(self,n_clusters: int, pipe: Pipeline, centers, cluster_target,  *args, **kwargs):
@@ -66,7 +69,7 @@ class ClusteringPreprocessor(BasePreprocessor):
         self.centers = centers or None 
 
     def preprocess_data(self, data: pd.DataFrame, is_training: bool) -> pd.DataFrame:
-        data = super().preprocess_data(data.copy())
+        data = super().preprocess_data(data.copy(), is_training=is_training)
         data = convert_polyline_to_geojson_format(data=data, name_column='dest_point')
         data = extract_lat_lon(data,'dest_point')
         data = self.preprocess_cluster(data=data.copy(), n_clusters=self.n_clusters, col_name = self.cluster_target, 
